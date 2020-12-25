3183d7adf3d36738af6164bf8f7e72fa26b5fb10
Ankit Jain <ankit.jain67@t-mobile.com>
2020-12-25 00:42:46 -0800

    css changes

--- scripts/search.js
+++ scripts/search.js
@@ -57,8 +57,8 @@ function showLoader() {
 }
 
 function formatPhoneNumber(phoneNumberString) {
-    var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
-    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+    var cleanedPhoneNumbers = ('' + phoneNumberString).replace(/\D/g, '')
+    var match = cleanedPhoneNumbers.match(/^(\d{3})(\d{3})(\d{4})$/)
     if (match) {
         return '(' + match[1] + ') ' + match[2] + '-' + match[3]
     }
